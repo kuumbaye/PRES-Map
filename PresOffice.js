1112153759e89451193071ba0d8da14a696e662d
kuumbaye <bkuumba@asu.edu>
2017-07-10 15:18:14 -0700

    Adding User Info Placeholders

---
+++ PresOffice.js
@@ -0,0 +1,47 @@
+var imageOrig = document.getElementById("imageOrig");
+var state = "Orig";
+
+function openOrig() {
+    imageOrig.style.content = "url('4thPRESOffice0617.jpg')";
+    state = "Orig";
+}
+
+function openMobile() {
+    imageOrig.style.content = "url('4thPRESOffice0617Mobile.jpg')";
+    state = "Mobile";
+}
+
+function openPrinters() {
+    imageOrig.style.content = "url('4thPRESOffice0617Printers.jpg')";
+    state = "Printers";
+}
+
+function openiPad() {
+    imageOrig.style.content = "url('4thPRESOffice0617iPad.jpg')";
+    state = "iPad";
+}
+
+function openPhone() {
+    imageOrig.style.content = "url('4thPRESOffice0617Phones.jpg')";
+    state = "Phone";
+}
+
+function openHotSpot() {
+    imageOrig.style.content = "url('4thPRESOffice0617HotSpots.jpg')";
+    state = "HotSpot";
+}
+
+function openNetworked() {
+    imageOrig.style.content = "url('4thPRESOffice0617NetworkedP.jpg')";
+    state = "Networked";
+}
+
+function openPersonal() {
+    imageOrig.style.content = "url('4thPRESOffice0617PersonalP.jpg')";
+    state = "Personal";
+}
+
+function openAll() {
+    imageOrig.style.content = "url('4thPRESOffice0617All.jpg')";
+    state = "All";
+}
